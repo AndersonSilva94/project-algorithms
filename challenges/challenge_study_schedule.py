09f139cfdb5068d7e621b81c9475e0e42fb89440
Anderson Silva <andssilva.23@gmail.com>
2022-05-01 15:13:19 -0400

    fix(req 1): :bug: corrige erro de flake8

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -26,7 +26,8 @@ def study_schedule(permanence_period, target_time):
     students = 0
     for start_period, end_period in permanence_period:
         "istanceof é um método que verifica se a variável possui o tipo"
-        if not isinstance(start_period, int) or not isinstance(end_period, int):
+        if not isinstance(start_period, int) \
+                or not isinstance(end_period, int):
             return None
         if start_period <= target_time <= end_period:
             students += 1
